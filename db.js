caecaf0c4a431eab742444078ed24c81d725ec96
Focrult <focrult@gmail.com>
2023-02-12 19:34:49 +0800

    Changed some code around. Added additional middleware to the server.js

--- db.js
+++ db.js
@@ -1,12 +1,4 @@
-const mysql = require('mysql2');
 
-//create connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    password: 'Not showing my password :P',
-    user: 'root',
-    database: 'mydb'
-})
 
 connection.connect();
 
--- server.js
+++ server.js
@@ -1,3 +1,38 @@
-//import db
+//Declarations
+const express = require('express');
+const mysql = require('mysql2');
+const dbModule = require('./db.js');
 
-const db = require('./db.js');
+const PORT = process.env.PORT || 3001;
+const app = express();
+//Express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    password: 'Not showing my password :P',
+    user: 'root',
+    database: 'mydb'
+},
+console.log("Connected to the mydb database")
+);
+
+
+
+
+
+
+
+
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
