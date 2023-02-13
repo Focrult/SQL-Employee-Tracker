f106c81fd94073db4aebc8de35648022a42df91a
Focrult <focrult@gmail.com>
2023-02-13 13:52:10 +0800

    Added some prompts and going to manage through user choice through a switch statement. Additionally reinstalled inquirer for a more appropriate version.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Declarations
 const express = require('express');
+const inquirer= require('inquirer');
 const mysql = require('mysql2');
 const dbModule = require('./db.js');
 
@@ -21,10 +22,60 @@ console.log("Connected to the mydb database")
 );
 
 
+const Prompts = () => {
+    inquirer.prompt ([
+        {
+        type: 'list',
+        name: 'Start',
+        message: 'Select what you want to do:',
+        choices: [
+            "View all Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Exit"
+        ]
+        }
+    ]).then(choice1 => {
+        const verify = choice1.Start;
+        switch (verify){
+            case "View all Employees":
+
+            break;
+            case "Add Employee":
+
+            break;
+            case "Update Employee Role":
+
+            break;
+            case "View All Roles":
+
+            break;
+            case "Add Role":
+
+            break;
+            case "View All Departments":
+
+            break;
+            case "Add Department":
+
+            break;
+            case "Exit":
+
+            break;
+            default:
+                console.log("Error");
+        }
+    })
 
 
+}
 
 
+Prompts();
 
 
 
