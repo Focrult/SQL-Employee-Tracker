0ad3951446b03d812becdaa258b921ee7d6e1b4f
Focrult <focrult@gmail.com>
2023-02-13 14:50:17 +0800

    Added functions with some text to verify that the input works successfully, and it does!

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ connection.connect(err => {
   console.log('Connected to the database.');
 });
 
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
 const Prompts = () => {
   inquirer.prompt ([
     {
@@ -42,27 +50,35 @@ const Prompts = () => {
       switch (verify){
           case "View all Employees":
               // Logic for handling "View all Employees" goes here
+                ViewEmployees();
               break;
           case "Add Employee":
               // Logic for handling "Add Employee" goes here
+                AddEmployees();
               break;
           case "Update Employee Role":
               // Logic for handling "Update Employee Role" goes here
+                UpdateEmployees()
               break;
           case "View All Roles":
               // Logic for handling "View All Roles" goes here
+                ViewRoles();
               break;
           case "Add Role":
               // Logic for handling "Add Role" goes here
+                AddRole();
               break;
           case "View All Departments":
               // Logic for handling "View All Departments" goes here
+                ViewDepartments();
               break;
           case "Add Department":
               // Logic for handling "Add Department" goes here
+                AddDepartments();
               break;
           case "Exit":
               // Logic for handling "Exit" goes here
+                Exit();
               break;
           default:
               console.log("Error");
@@ -72,10 +88,29 @@ const Prompts = () => {
 
 Prompts();
 
-app.use((req, res) => {
-  res.status(404).end();
-});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+//Functions Below
+const ViewEmployees = () => {
+    console.log("View Employee");
+}
+const AddEmployees = () => {
+    console.log("Add Employee");
+}
+const UpdateEmployees = () => {
+    console.log("Update Employee");
+}
+const ViewRoles = () => {
+    console.log("View Role");
+}
+const AddRole = () => {
+    console.log("Add a role")
+}
+const ViewDepartments = () => {
+    console.log("view all departments")
+}
+const AddDepartments = () => {
+    console.log("Add a department");
+}
+const Exit = () => {
+    console.log("Exit");
+}
